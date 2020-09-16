8942ee64f3b9d4c3e42ba050e70bc72de426399e
Andrea <aganesh0988@gmail.com>
2020-09-16 12:43:48 -0500

    created Calculator component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Calculator from './Calculator.js';
 
 const App = () => (
   <div className="App">
---
+++ src/Calculator.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Time for math!</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
