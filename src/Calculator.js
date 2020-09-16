ede3dffbe1a8049bcddaf6fa3d8ad8163cff9f68
Andrea <aganesh0988@gmail.com>
2020-09-16 15:22:41 -0500

    added calculator buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -20,12 +20,44 @@ class Calculator extends React.Component {
     this.setState({ num2 })
   };
 
+  adding = () => {
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result })
+  }
+
+  subtracting = () => {
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result })
+  }
+
+  multiplying = () => {
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result })
+  }
+
+  dividing = () => {
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result })
+  }
+
+  clearInput = () => {
+    const num1 = '';
+    const num2 = '';
+    const result = 0;
+    this.setState({ num1, num2, result })
+  }
+
   render() {
     return (
       <div>
         <h1>Result: {this.state.result}</h1>
         <input onChange={this.handleFirstNum} value={this.state.num1} placeholder="First number" />
         <input onChange={this.handleSecondNum} value={this.state.num2} placeholder="Second number" />
+        <button onClick={this.adding}>Add</button>
+        <button onClick={this.subtracting}>Subtract</button>
+        <button onClick={this.multiplying}>Multiply</button>
+        <button onClick={this.dividing}>Divide</button>
+        <button onClick={this.clearInput}>Clear Fields</button>
       </div>
     );
   }
